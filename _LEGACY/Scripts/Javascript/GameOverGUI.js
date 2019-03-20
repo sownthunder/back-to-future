eb2dc9831c40ed4d3407987a185507b7feefbd1f
sownthunder <dbates117@gmail.com>
2019-03-20 00:29:15 -0400

    upload java files to LEGACY folder

---
+++ _LEGACY/Scripts/Javascript/GameOverGUI.js
@@ -0,0 +1,37 @@
+﻿#pragma strict
+
+// declare custom GUI style
+public var gameOverGUIStyle : GUIStyle;
+
+// Use this for pre-initialization
+function Awake ()
+{
+
+	
+
+}
+
+// Use this for initialization
+function Start () 
+{
+
+	
+
+}
+
+// Called once per frame
+function Update () 
+{
+
+	
+
+}
+
+// Use this for Graphical User Interface
+function OnGUI ()
+{
+
+	// display the fact that the user is a total noob 
+	GUI.Box(Rect ((Screen.width / 2), ((Screen.height / 10) * 9), (Screen.width / 5), 100), "YOU LOSE LOL!", gameOverGUIStyle);
+
+}
---
+++ _LEGACY/Scripts/Javascript/Tile.js
@@ -0,0 +1,44 @@
+﻿#pragma strict
+
+// Different types of tiles
+// ROAD
+// POTHOLE (ROAD)
+// SIDEWALK
+// GRASS
+// BUILDING
+// FLOOR
+// COUNTER
+// CRATE
+
+// A;; tje booleans / possible "type" of 'myTyle' there can be
+var isRoad : boolean = false;
+var isPotHole : boolean = false;
+var isSideWalk : boolean = false;
+var isGrass : boolean = false;
+var isBuilding : boolean = false;
+var isFloor : boolean = false;
+var isCounter : boolean = false; 
+var isCrate : boolean = false; 
+
+// use this for pre-initialization
+function Awake ()
+{
+
+	
+
+}
+
+// use this for initialization
+function Start () 
+{
+
+	
+
+}
+
+function Update () 
+{
+
+	
+
+}
